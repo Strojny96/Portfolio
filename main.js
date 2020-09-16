cfe64348d15e7a00f22aa2770b8f14c1cc07e080
Strojny96 <71374378+Strojny96@users.noreply.github.com>
2020-09-16 11:05:03 +0200

    Dekstop version of the site
    First version of the site, next step is to change it to RWD or add media queries for tablet/mobile

---
+++ main.js
@@ -0,0 +1,28 @@
+const menuToggler = document.querySelector('.menu-toggler');
+const topNav = document.querySelector('.top-nav');
+const navLink = document.querySelectorAll('.top-nav .nav-link');
+const scroll = new SmoothScroll('a[href*="#"]', {
+    speed: 1000
+});
+
+const menu = () => {
+    menuToggler.addEventListener('click', () => {
+        menuToggler.classList.add('open');
+        topNav.classList.add('open');
+        menuToggler.addEventListener('click', () => {
+            menuToggler.classList.remove('open');
+            topNav.classList.remove('open');
+            menu();
+        })
+    })
+
+    /*navLink.forEach(navLink => {
+        navLink.addEventListener('click', () => {
+            menuToggler.classList.toggle('open');
+            topNav.classList.toggle('open');
+            menu();
+        })
+    });*/
+
+}
+menu();
